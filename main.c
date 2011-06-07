90ba0a498c8bf6fbb76d1ad39af76158dc7fe4a9
Scott Ellis <scottellis.developer@gmail.com>
2011-06-07 06:21:46 -0400

    Smaller pics for testing

--- main.c
+++ main.c
@@ -17,10 +17,10 @@
 #include <opencv\highgui.h>
 
 #define RANDOM_SEED 0x1337
-#define IMAGE_WIDTH 1024
-#define IMAGE_HEIGHT 768
+#define IMAGE_WIDTH 512
+#define IMAGE_HEIGHT 384
 
-#define BACKGROUND_COLOR 120
+#define BACKGROUND_COLOR 180
 
 #define ESCAPE_KEY 27
 
@@ -37,25 +37,28 @@ int generate_cal_image(const char *dir, int num_cal_images);
 void draw_loop(const char *dir, int start_image_num, int num_images);
 void draw_shapes(IplImage *img);
 int random_range(int low, int high);
-void draw_circles(IplImage *img, int min_cnt, int max_cnt);
-void draw_ellipses(IplImage *img, CvScalar color, int min_cnt, int max_cnt);
+void draw_circles(IplImage *img, int min_cnt, int max_cnt, int with_centers);
+void draw_ellipses(IplImage *img, int min_cnt, int max_cnt);
 void draw_squares(IplImage *img, CvScalar color, int min_cnt, int max_cnt);
 void draw_rectangles(IplImage *img, CvScalar color, int min_cnt, int max_cnt);
 
 void save_image(const char *dir, IplImage *img, int index);
 
-const char project_dir[] = "C:\\Users\\scott\\My Documents\\FluidData\\ShapeGenBW\\Circles_2\\";
+const char project_dir[] = "C:\\Users\\scott\\My Documents\\FluidData\\ShapeGenBW\\Circle_Ellipse_1\\";
 
 int main(int argc, char **argv)
 {
+	int num_cal_images = 32;
+	int num_images = 100;
+
 	init_rng();
 		
 	cvNamedWindow("shapegen", CV_WINDOW_AUTOSIZE);
 	cvMoveWindow("shapegen", 10, 10);
 
-	if (generate_cal_image(project_dir, 32)) {
-		draw_loop(project_dir, 32, 100);
-		//draw_loop(NULL);
+	if (generate_cal_image(project_dir, num_cal_images)) {
+		draw_loop(project_dir, num_cal_images, num_images);
+		//draw_loop(NULL, num_cal_images, 20);
 	}
 
 	return 0;
@@ -83,7 +86,7 @@ void save_image(const char *dir, IplImage *img, int index)
 int generate_cal_image (const char *dir, int num_cal_images)
 {
 	int key, i;
-	CvScalar bkgd_color, zero, one;
+	CvScalar bkgd_color, one;
 	IplImage *img, *cal_img, *sum_img, *mask;
 	CvSize size = { IMAGE_WIDTH, IMAGE_HEIGHT };
 
@@ -119,15 +122,14 @@ int generate_cal_image (const char *dir, int num_cal_images)
 	one.val[0] = 1.0;
 	cvSet(mask, one, NULL);
 
-	zero.val[0] = 0.0;
-	cvSet(sum_img, zero, NULL);
+	cvZero(sum_img);
 	
 	for (i = 0; i < num_cal_images; i++) {		
 		draw_shapes(img);
 		save_image(dir, img, i);
 
 		cvAcc(img, sum_img, mask);
-		cvSet(cal_img, zero, NULL);
+		cvZero(cal_img);
 
 		cvConvertScale(sum_img, cal_img, 1.0 / (i + 1), 0);
 		cvShowImage("shapegen", cal_img);
@@ -181,17 +183,15 @@ void draw_loop(const char *dir, int start_image_num, int num_images)
 
 void draw_shapes(IplImage *img)
 {
-	CvScalar bkgd_clr;
-
-	bkgd_clr.val[0] = BACKGROUND_COLOR;
-	bkgd_clr.val[1] = BACKGROUND_COLOR;
-	bkgd_clr.val[2] = BACKGROUND_COLOR;
+	CvScalar bkgd_clr = cvScalarAll(BACKGROUND_COLOR);
 
 	cvSet(img, bkgd_clr, 0);
-	draw_circles(img, 0, 5);
+	draw_circles(img, 0, 3, 0);
+	draw_circles(img, 0, 2, 1);
+	draw_ellipses(img, 0, 3);
 }
 
-void draw_circles(IplImage *img, int min_cnt, int max_cnt)
+void draw_circles(IplImage *img, int min_cnt, int max_cnt, int with_centers)
 {
 	int i, val, cnt;
 	CvPoint center;
@@ -201,42 +201,44 @@ void draw_circles(IplImage *img, int min_cnt, int max_cnt)
 	cnt = random_range(min_cnt, max_cnt);
 
 	for (i = 0; i < cnt; i++) {
-		val = random_range(BACKGROUND_COLOR - 60, BACKGROUND_COLOR - 30);
-		color.val[0] = val;
-		color.val[1] = val;
-		color.val[2] = val;
+		val = random_range(BACKGROUND_COLOR - 60, BACKGROUND_COLOR - 20);
+		color = cvScalarAll(val);
 
-		center.x = random_range(50, 950);
-		center.y = random_range(50, 700);
-		radius = random_range(20, 60);
+		center.x = random_range(50, IMAGE_WIDTH - 50);
+		center.y = random_range(50, IMAGE_HEIGHT - 50);
+		radius = random_range(10, 40);
 		cvCircle(img, center, radius, color, 1, CV_AA, 0);
 
-		val = random_range(BACKGROUND_COLOR + 10, BACKGROUND_COLOR + 30);
-		color.val[0] = val;
-		color.val[1] = val;
-		color.val[2] = val;
+		if (with_centers) {
+			val = random_range(BACKGROUND_COLOR + 8, BACKGROUND_COLOR + 30);
+			color = cvScalarAll(val);
 
-		val = random_range(8, 16);
-		cvCircle(img, center, val, color, CV_FILLED, CV_AA, 0);
+			val = random_range(4, radius);
+			cvCircle(img, center, val, color, CV_FILLED, CV_AA, 0);
+		}
 	}	
 }
 
-void draw_ellipses(IplImage *img, CvScalar color, int min_cnt, int max_cnt)
+void draw_ellipses(IplImage *img, int min_cnt, int max_cnt)
 {
-	int i, cnt;
+	int i, val, cnt;
 	CvPoint center;
 	CvSize axes;
 	double angle;
+	CvScalar color;
 	
 	cnt = random_range(min_cnt, max_cnt);
 
 	for (i = 0; i < cnt; i++) {
-		center.x = random_range(50, 950);
-		center.y = random_range(50, 700);
-		axes.width = random_range(5, 50);
-		axes.height = random_range(5, 50);
+		val = random_range(BACKGROUND_COLOR - 60, BACKGROUND_COLOR - 20);
+		color = cvScalarAll(val);
+
+		center.x = random_range(50, IMAGE_WIDTH - 50);
+		center.y = random_range(50, IMAGE_HEIGHT - 50);
+		axes.width = random_range(5, 40);
+		axes.height = random_range(5, 40);
 		angle = random_range(0, 180);
-		cvEllipse(img, center, axes, angle, 0.0, 360.0, color, CV_FILLED, 8, 0);
+		cvEllipse(img, center, axes, angle, 0.0, 360.0, color, 1, CV_AA, 0);
 	}	
 }
 
@@ -248,8 +250,8 @@ void draw_squares(IplImage *img, CvScalar color, int min_cnt, int max_cnt)
 	cnt = random_range(min_cnt, max_cnt);
 
 	for (i = 0; i < cnt; i++) {
-		topLeft.x = random_range(50, 950);
-		topLeft.y = random_range(50, 700);
+		topLeft.x = random_range(50, IMAGE_WIDTH - 50);
+		topLeft.y = random_range(50, IMAGE_HEIGHT - 50);
 		side = random_range(5, 50);
 		bottomRight.x = side + topLeft.x;
 		bottomRight.y = side + topLeft.y;
@@ -265,8 +267,8 @@ void draw_rectangles(IplImage *img, CvScalar color, int min_cnt, int max_cnt)
 	cnt = random_range(min_cnt, max_cnt);
 
 	for (i = 0; i < cnt; i++) {
-		topLeft.x = random_range(50, 950);
-		topLeft.y = random_range(50, 700);
+		topLeft.x = random_range(50, IMAGE_WIDTH - 50);
+		topLeft.y = random_range(50, IMAGE_HEIGHT - 50);
 		height = random_range(5, 50);
 		width = random_range(5, 50);
 		bottomRight.x = width + topLeft.x;
